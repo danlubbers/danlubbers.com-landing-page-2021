04ee82590509bc7560019ec9738bf017cfb321ab
Dan Lubbers <lubbers.dan@gmail.com>
2021-09-21 18:55:26 -0400

    Added React Ga and events. Changed reset file to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { ReactComponent as SVGLogo } from "./assets/logos/danlubbers_logo_#A52A25_outlined.svg";
 import "./App.scss";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-1733817-2");
 
 function App() {
   return (
     <main className="App">
+      {ReactGA.event({
+        category: "[danlubbers.com]",
+        action: "[danlubbers.com]: User landed on landing page",
+      })}
+      ;
       <SVGLogo className="logo" />
       <div className="btn-container">
         <a
@@ -11,14 +19,36 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <button className="photo-btn">Photography / Retouching</button>
+          <button
+            className="photo-btn"
+            onClick={() =>
+              ReactGA.event({
+                category: "[danlubbers.com]",
+                action:
+                  "[danlubbers.com]: User clicked Photographs/Retouching site link",
+              })
+            }
+          >
+            Photography / Retouching
+          </button>
         </a>
         <a
           href="https://webdev.danlubbers.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <button className="webDev-btn">Web Development</button>
+          <button
+            className="webDev-btn"
+            onClick={() =>
+              ReactGA.event({
+                category: "[danlubbers.com]",
+                action:
+                  "[danlubbers.com]: User clicked Web Development site link",
+              })
+            }
+          >
+            Web Development
+          </button>
         </a>
       </div>
     </main>
